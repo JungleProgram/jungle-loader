1496607f5e9d636454fddd70ea2ce7b84aa86689
Louis <louis@lacompany.com>
2018-08-28 20:27:10 +0200

    lint fixes and lottie template with loading assets

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,54 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view></router-view>
+    <lottie :options="defaultOptions" :height="400" :width="400" v-on:animCreated="handleAnimation"/>
+      <div>
+          <p>Speed: x{{animationSpeed}}</p>
+          <input type="range" value="1" min="0" max="3" step="0.5"
+                  v-on:change="onSpeedChange" v-model="animationSpeed">
+      </div>
+      <button v-on:click="stop">stop</button>
+      <button v-on:click="pause">pause</button>
+      <button v-on:click="play">play</button>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'app'
-}
+  import Lottie from './lottie.vue'
+  import * as animationData from './assets/logo_loading.json'
+
+  export default {
+    name: 'app',
+    components: {
+      'lottie': Lottie
+    },
+    data () {
+      return {
+        defaultOptions: {animationData: animationData},
+        animationSpeed: 1
+      }
+    },
+    methods: {
+      handleAnimation: function (anim) {
+        this.anim = anim
+      },
+
+      stop: function () {
+        this.anim.stop()
+      },
+
+      play: function () {
+        this.anim.play()
+      },
+
+      pause: function () {
+        this.anim.pause()
+      },
+
+      onSpeedChange: function () {
+        this.anim.setSpeed(this.animationSpeed)
+      }
+    }
+  }
 </script>
 
 <style>
